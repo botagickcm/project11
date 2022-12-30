7e455cc00dc1f728028f1076b500bc24b6bceb2f
botagickcm <kanitooo0015@gmail.com>
2022-12-30 12:25:00 +0600

    college ozgerdi

--- Task8_11.java
+++ Task8_11.java
@@ -1,6 +1,6 @@
 class Task8_11{
 	public static void main(String[] args) {
-		String word="salem alem";
+		String word="salem college";
 		int length=word.length();
 		System.out.println(length);
 		
